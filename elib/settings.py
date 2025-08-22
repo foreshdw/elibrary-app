cc336a23b5041e82226f2e51863f0ec9fd2aa2b8
Farid Adika <Faridadika83@gmail.com>
2025-08-22 18:57:50 +0700

    add some changes

--- elib/settings.py
+++ elib/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-estmh=q1qjxy3oquuj1g+wd5-2*d(kq(e#k4%tv6$l7^vbkikr'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback_key_for_dev_only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
